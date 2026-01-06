843b0ae851f4f09c4456f75be144633d7c42bd58
Anvesh-999 <anvesh.a9100@gmail.com>
2026-01-06 22:07:54 +0530

    optimized bubble sort code

--- Sorting-Algorithms/BubbleSort.java
+++ Sorting-Algorithms/BubbleSort.java
@@ -1,13 +1,18 @@
 public class BubbleSort {
     public static void bubbleSort(int arr[]){
-        for(int i=0;i<arr.length-1;i++){
-            for(int j=0;j<arr.length-i-1;j++){
+        int n = arr.length;
+        boolean swapped;
+        for(int i=0;i<n-1;i++){
+            swapped=false;
+            for(int j=0;j<n-i-1;j++){
                 if(arr[j]>arr[j+1]){
                     int temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
+                    swapped=true;
                 }
             }
+            if(!swapped) break;
         }
     }
     public static void printArr(int arr[]){
