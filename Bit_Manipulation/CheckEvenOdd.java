5769b8590302493e10cb39638a6b484a85a4b3d4
Anvesh-999 <anvesh.a9100@gmail.com>
2026-02-25 19:03:31 +0530

    Check even or odd using bit & operator

---
+++ Bit_Manipulation/CheckEvenOdd.java
@@ -0,0 +1,13 @@
+public class CheckEvenOdd {
+    public static void check(int n){
+        if((n&1)==0){
+            System.out.println("Even number");
+        }else{
+            System.out.println("Odd number");
+        }
+    }
+    public static void main(String[] args) {
+        check(19);
+        check(80);
+    }
+}
