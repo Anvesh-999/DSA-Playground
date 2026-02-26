92679fd44a289f4b89b4cb3117cc7bac14475b7f
Anvesh-999 <anvesh.a9100@gmail.com>
2026-02-26 21:33:20 +0530

    counting set bits

--- Bit_Manipulation/BitOperation.java
+++ Bit_Manipulation/BitOperation.java
@@ -35,6 +35,19 @@ public class BitOperation {
         int bitMask=a|b;
         return n & bitMask;
     }
+    public static boolean isPowerOfTwo(int n){
+        return n>0 && (n&(n-1))==0;
+    }
+    public static int CountSetBits(int n){
+        int count=0;
+        while(n>0){
+            if((n&1)!=0){
+                count++;
+            }
+            n=n>>1;
+        }
+        return count;
+    }
     public static void main(String[] args) {
         System.out.println(getithBit(5, 2));
         System.out.println(setithBit(10, 2));
@@ -42,5 +55,7 @@ public class BitOperation {
         System.out.println(updateIthBit(10, 2,1));
         System.out.println(clearLastIthBits(15, 2));
         System.out.println(clearBitsInRange(10,2,4));
+        System.out.println(isPowerOfTwo(16));
+        System.out.println(CountSetBits(15));
     }
 }
