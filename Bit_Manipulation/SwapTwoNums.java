b5548b8f3e527075f3f901aae3d16b1e04d98d45
Anvesh-999 <anvesh.a9100@gmail.com>
2026-02-27 22:19:42 +0530

    SwapTwoNums without using xor operator

---
+++ Bit_Manipulation/SwapTwoNums.java
@@ -0,0 +1,10 @@
+public class SwapTwoNums {
+    public static void main(String []args){
+        int x=3,y=4;
+        System.out.println("Before swap : x= "+ x +"and y= "+y);
+        x=x^y;
+        y=x^y;
+        x=x^y;
+        System.out.println("After swap : x= "+x+"and y= "+y);
+    }
+}
