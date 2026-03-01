8257c5ecf5483ab03ce0403b204334857e5b77c9
Anvesh-999 <anvesh.a9100@gmail.com>
2026-03-01 21:11:09 +0530

    Factorial using recursion

---
+++ Recursion/Factorial.java
@@ -0,0 +1,9 @@
+public class Factorial {
+    public static int calculateFactorial(int n){
+        if(n==1) return 1;
+        return n*calculateFactorial(n-1);
+    }
+    public static void main(String[] args) {
+        System.out.println(calculateFactorial(5));
+    }    
+}
