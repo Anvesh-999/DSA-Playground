f1feb9b29361311624e04b1ea74425d7719aa22c
Anvesh-999 <anvesh.a9100@gmail.com>
2026-03-02 13:16:23 +0530

    power calculate using recursion

---
+++ Recursion/Power.java
@@ -0,0 +1,16 @@
+public class Power {
+    public static int power(int x,int n){
+        if(n==0) return 1;
+        return x*power(x, n-1);
+    }
+    public static int OptimizedPower(int x,int n){
+        if(n==0) return 1;
+        int halfPower=OptimizedPower(x, n/2);
+        int halfPowerSq=halfPower*halfPower;
+        if(n%2!=0) halfPowerSq= x*halfPowerSq;
+        return halfPowerSq;
+    }
+    public static void main(String[] args) {
+        System.out.println(OptimizedPower(2, 3));
+    }    
+}
